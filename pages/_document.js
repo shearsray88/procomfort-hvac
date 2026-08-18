34bb4b4d8a05197395f838295c1dc6302112ea8f
Raymond <shearsray88@gmail.com>
2026-08-18 19:13:45 +0700

    fix: brand color contrast on primary buttons (WCAG AA)

--- pages/_document.js
+++ pages/_document.js
@@ -5,8 +5,6 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta charSet="utf-8" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
 
         {/* Primary Meta Tags */}
